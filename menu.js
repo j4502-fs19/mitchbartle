c7c5cc8a4b864b7945070bb3dd0a4caed8fc6fed
Mitchell Bartle <mitchbartle96@gmail.com>
2019-10-28 07:28:50 -0500

    added javascript and style

--- menu.js
+++ menu.js
@@ -1,1 +1,11 @@
-.Ready
+$(document).ready(function(){
+  $('.menu-trigger').click(function(){
+$('nav ul').slideToggle();
+});
+
+$(window).resize(function() {
+	   if (  $(window).width() > 500 ) {
+		$('nav .header .container2').removeAttr('style');
+	 }
+	});
+});
